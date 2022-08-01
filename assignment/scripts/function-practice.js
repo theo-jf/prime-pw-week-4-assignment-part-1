dd40f1d09c764926a7a4f7a12eb8dd31043b051e
Theo Janke-Furman <tjankefurman@gmail.com>
2022-08-01 14:53:53 -0500

    Completed 2-6 in function-practice.js

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,30 +15,34 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return "Hello, " + `${name}` + "!";
 }
 // Remember to call the function to test
+console.log("Test - should say 'Hello name!', where 'name' is the variable entered into the function", helloName(Theo));
 
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondNumber ) {
   // return firstNumber + secondNumber;
+  return firstNumber + secondNumber;
 }
+console.log("Test - should return the sum of 33 and 17, being 50", addNumbers(33, 17));
 
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree( firstNumber, secondNumber, thirdNumber ){
+  return firstNumber * secondNumber * thirdNumber;
 }
-
+console.log("Test - should return the product of -1, 2, and 3, being -6", multiplyThree(-1, 2, 3));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
-  if ( number > 0 ){
-    return;
+  if (number > 0) {
+    return true;
+  } else {
+    return false;
   }
-    return;
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
@@ -49,9 +53,13 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
+let emptyArray = [];
+let animalArray = ["eagle", "salamander", "snake"]
 
+function getLast( array ) {
+  return (!array.length) ? "undefined" : array[array.length - 1];
 }
+console.log("Test - should return 'undefined' and 'snake' in that order", getLast(emptyArray), getLast(animalArray));
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
